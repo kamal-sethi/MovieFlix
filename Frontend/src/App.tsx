473b2fcce2bbaf0b46781792057fd62077cf7fd9
kamal-sethi <kamalsethi018@gmail.com>
2025-10-30 18:31:02 +0530

    initilized the react-vite app and installed tailwind css also

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="h-screen w-full bg-gradient-to-br from-[#0f0f0f] via-[#1a0000] to-[#b20710] flex items-center justify-center">
+     
+    </div>
+  );
+}
+
+export default App;
+
+
+
